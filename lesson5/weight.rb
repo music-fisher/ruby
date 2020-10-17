caf65d4c2b9cbce6a5368bd40c863097fc17b367
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-10-17 09:15:12 +0000

    [Start]2-2~8

---
+++ lesson5/weight.rb
@@ -0,0 +1,5 @@
+name = "A"
+weight = 50
+#下記実行不可。。
+puts name + "さんの体重は"+ weight.to_s + "kgです"　#この行を追加
+puts "#{name}さんの体重は#{weight}kgです"#この行を追加
---
+++ lesson8/greeting.rb
@@ -0,0 +1,6 @@
+def greeting(name)
+  return"Hello,#{name}!"#この行のnameは引数のname
+  "Good morning,#{name}!"
+end
+
+puts greeting('john')
